cd902d3b7e4361145a825239a73bae589f777e68
Ron Lane <theartfuldodger2012@gmail.com>
2024-03-28 15:33:20 -0400

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Ron Lane on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
